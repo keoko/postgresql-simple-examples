3986ae60122ee3669875bce7faddf63e0210ba69
natxo cabré <natxo.cabre@gmail.com>
2015-04-11 20:39:16 +0200

    init files

--- Main.hs
+++ Main.hs
@@ -1,3 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Database.PostgreSQL.Simple
+
+myconn :: ConnectInfo
+myconn = defaultConnectInfo {
+             connectUser = "natxo",
+             connectPassword = "natxo",
+             connectDatabase = "helloworld"}
+
 main :: IO ()
 main = do
-	putStrLn "hello world"
+   c <- connect myconn :: IO Connection
+   rs <- query_ c "select 2 + 4" :: IO [Only Int]
+   putStrLn $ "Result from database " ++ show (fromOnly $ head rs)
