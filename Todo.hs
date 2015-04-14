e621c2740da14badb0d4808b3573d1e113c782b0
natxo cabré <natxo.cabre@gmail.com>
2015-04-14 22:38:10 +0200

    added insert example

--- Todo.hs
+++ Todo.hs
@@ -10,6 +10,7 @@ module Todo where
 -- null values on title
 -- extract IO value
 import Data.Text
+import Data.Int (Int64)
 import Data.ByteString (ByteString)
 import Control.Applicative
 import Control.Monad.IO.Class (liftIO)
@@ -19,13 +20,16 @@ import Database.PostgreSQL.Simple.FromRow
 import Database.PostgreSQL.Simple.ToRow
 
 
-data TodoItem = TodoItem { id :: Int
-                         , title :: Text
-                         , completed :: Bool
+data TodoItem = TodoItem { todoId :: Int
+                         , todoTitle :: Text
+                         , todoCompleted :: Bool
                          } deriving Show
 
 instance FromRow TodoItem where
   fromRow = TodoItem <$> field <*> field <*> field
+
+instance ToRow TodoItem where
+  toRow t = [toField (todoId t), toField (todoTitle t), toField (todoCompleted t)]
 
 uri :: ByteString
 uri = "postgres://natxo@localhost/helloworld"
@@ -33,9 +37,12 @@ uri = "postgres://natxo@localhost/helloworld"
 allTodoItems :: Connection -> IO [TodoItem]
 allTodoItems c = query_ c "SELECT id, title, completed FROM todo"
 
-
 getTodoItem :: Connection -> Int -> IO [TodoItem]
-getTodoItem c id = query c "SELECT id, title, completed FROM todo WHERE id = ?" $ Only id 
+getTodoItem c id = query c "SELECT id, title, completed FROM todo WHERE id = ?" $ Only id
+
+addTodoItem :: Connection -> TodoItem -> IO Int64
+addTodoItem c i = execute c "INSERT INTO todo (id, title, completed) VALUES (?, ?, ?)" i
+
 
 main :: IO ()
 main = do
@@ -43,3 +50,4 @@ main = do
   putStrLn "all todo"
   mapM_ (putStrLn . show) =<< allTodoItems conn
   mapM_ (putStrLn . show) =<< getTodoItem conn 2
+  putStrLn . show =<< addTodoItem conn TodoItem {todoId=2, todoTitle="test", todoCompleted=False}
